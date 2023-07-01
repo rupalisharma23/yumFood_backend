3a9048254e1309d89bb38bc47da58c35ab730bec
rupalisharma23 <rupalisharma232002@gmail.com>
2023-07-01 21:02:38 +0530

    change

--- src/Components/NavBar.jsx
+++ src/Components/NavBar.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { NavLink } from 'react-router-dom';
 import './NavBar.css';
 import ShoppingCartIcon from '@mui/icons-material/ShoppingCart';
+import LogoutIcon from '@mui/icons-material/Logout';
 
 const Navigation = (props) => {
     return (
@@ -15,8 +16,14 @@ const Navigation = (props) => {
                 <NavLink to="/MyOrders" className="nav-button">
                     My Orders
                 </NavLink>
-                <NavLink className="nav-button" onClick={() => { props.setCartFlag(true) }}>
-                    <ShoppingCartIcon className="white-icon" />
+                {window.location.pathname !== '/MyOrders' && <NavLink className="nav-button" onClick={() => { props.setCartFlag(true) }}>
+                    <div style={{position:'relative'}}>
+                        <ShoppingCartIcon className="white-icon" />
+                        {(props.cartItems.length > 0 )&& <div className='cartCount'>{props.cartItems.length}</div>}
+                        </div>
+                </NavLink>}
+                <NavLink className="nav-button" to="/" onClick={() => { localStorage.clear() }}>
+                    <LogoutIcon className="white-icon" />
                 </NavLink>
             </div>
         </nav>
