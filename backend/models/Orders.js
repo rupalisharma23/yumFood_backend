11aa137e1e86d6572eca957cf82af533c8b576a2
rupalisharma23 <rupalisharma232002@gmail.com>
2023-07-01 11:55:26 +0530

    changes

--- backend/models/Orders.js
+++ backend/models/Orders.js
@@ -15,3 +15,5 @@ const foodOrders = new Schema({
 });
 
 module.exports = mongoose.model("Orders", foodOrders)
+
+// in this case, the collection in the database will be named "orders" (all lowercase and pluralized) even though you used "Orders" as the model name. MongoDB automatically pluralizes the model name to create the corresponding collection name.
